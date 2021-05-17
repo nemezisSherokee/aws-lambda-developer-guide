34109686d8d5a0f61cff102845ec59661556cb9f
JOSEPH HERMANN KAMDOUM NGADJUI <kamdoum_ngadjui@siemens.com>
2021-05-17 13:53:57 +0200

    add parent gitlab-ci

--- sample-apps/blank-java/1-create-bucket.sh
+++ sample-apps/blank-java/1-create-bucket.sh
@@ -3,3 +3,4 @@ BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \
 BUCKET_NAME=lambda-artifacts-$BUCKET_ID
 echo $BUCKET_NAME > bucket-name.txt
 aws s3 mb s3://$BUCKET_NAME
+
